cbfceb197ea50eced8b30f9eccc3bcb3c6d24622
Thiago Bomfim <tbomfim@atlassian.com>
2017-05-01 17:18:04 +1000

    exercise 1-2 completed

---
+++ src/com/ocajexam/tutorial/GreetingsUniverse.java
@@ -0,0 +1,11 @@
+package com.ocajexam.tutorial;
+import com.ocajexam.tutorial.planets.*;
+
+public class GreetingsUniverse {
+	public static void main(String[] args) {
+		Earth e = new Earth();
+		Mars m = new Mars();
+		Venus v = new Venus();
+       		System.out.println("Greetings, Universe!");
+	} 
+}
